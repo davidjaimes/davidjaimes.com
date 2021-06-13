212bd2c530670a0d43d1455b73783a33400023a6
David Jaimes <github@davidjaimes.com>
2021-06-13 16:18:44 -0700

    add bootstrap and markdown plugins

---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+module.exports = {
+	siteMetadata: {siteUrl: `https://davidjaimes.com`},
+	plugins: [
+		{
+	    	resolve: `gatsby-plugin-sharp`,
+	    	options: {
+				useMozJpeg: false,
+				stripMetadata: false,
+				defaultQuality: 50,
+	    	},
+	 	},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages/`,
+			},
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images/`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				remarkPlugins: [require(`remark-math`)],
+    			rehypePlugins: [require(`rehype-katex`)],
+				defaultLayouts: {default: require.resolve(`./src/components/homepage.jsx`)},
+				extensions: [`.mdx`, `.md`],
+				plugins: [`gatsby-remark-images`],
+				gatsbyRemarkPlugins: [
+					{
+				    	resolve: `gatsby-remark-prismjs`,
+				    	options: {
+				      		classPrefix: 'language-',
+				      		inlineCodeMarker: null
+				    	}
+				  	},
+					{
+	            		resolve: `gatsby-remark-images`,
+	            		options: {
+	            			maxWidth: 768,
+	            			showCaptions: false,
+	            			markdownCaptions: true,
+	            		},
+	          		},
+				]
+			},
+		},
+	],
+}
