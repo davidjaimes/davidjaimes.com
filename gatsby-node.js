a48fbe38a39966e839f8b9bc07e2ff3bf52ade93
David Jaimes <dj@davidjaimes.com>
2021-12-18 12:35:51 -0800

    Mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
-  if (node.internal.type === `MarkdownRemark`) {
+  if (node.internal.type === `Mdx`) {
     const parent = getNode(node.parent);
     let collection = parent.sourceInstanceName;
     createNodeField({
