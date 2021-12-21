4cdeb654bb2700a615b8c34a9e87c056420766f6
David Jaimes <dj@davidjaimes.com>
2021-12-20 21:19:45 -0800

    yay, i got query to work

--- src/components/macOS-cards.jsx
+++ src/components/macOS-cards.jsx
@@ -1,49 +1,13 @@
-// React Dependencies
-import { graphql } from 'gatsby'
-import React from 'react'
-
-// Bootstrap Dependencies
-import {Card, Media, Badge, Image} from 'react-bootstrap'
-import {FaBook} from 'react-icons/fa'
-
-// Images
-import systemPreferences from "../images/macOS/01-system-preferences.png"
-import Xcode from "../images/macOS/02-xcode-app.png"
-import Homebrew from "../images/macOS/03-homebrew.png"
-import SublimeText from "../images/macOS/04-sublime-text.png"
-import HyperJS from "../images/macOS/05-hyper-terminal.png"
+import React from "react"
+import { Link } from "gatsby"
 
 
-export default function App(data) {
+const PostLink = ({ post }) => (
+  <div>
+    <Link to={post.frontmatter.path}>
+      {post.frontmatter.title} ({post.frontmatter.date})
+    </Link>
+  </div>
+)
 
-	const post = data.allMdx
-
-	return (
-
-		<div>
-			<h1>{post}</h1>
-		</div>
-	)
-
-}
-
-export const pageQuery = graphql`
-query {
-	allMdx(
-	  filter: {fields: {collection: {eq: "pages"}}}
-	  sort: {fields: [frontmatter___date], order: DESC}
-	) {
-	  edges {
-		node {
-		  frontmatter {
-			date(formatString: "MMM DD, YYYY")
-			description
-			subtitle
-			title
-		  }
-		  fileAbsolutePath
-		}
-	  }
-	}
-  }
-`
+export default PostLink
--- src/pages/macOS/index.jsx
+++ src/pages/macOS/index.jsx
@@ -1,43 +1,32 @@
-// React Dependencies
-import React from "react";
-import {Helmet} from "react-helmet";
-
-// Bootstrap Dependencies
-import {Container, Row, Col} from 'react-bootstrap'
-
-// Import Templates
-import Navigation from "../../components/navigation"
-import CardIndex from "../../components/macOS-cards"
-import Footer from "../../components/footer"
+import React from "react"
+import { graphql } from "gatsby"
+import PostLink from "../../components/macOS-cards"
 
 
-export default function Index() {
-	return (
-		<React.Fragment>
-
-			<Helmet>
-				<html lang='en-us'/>
-      			<meta charset="utf-8"/>
-      			<meta name="viewport" content="width=device-width, initial-scale=1"/>
-      			<meta name="description" content="Utilization of Data Analysis and Visualization Tools: Discover the many different ways to collect, analyze, and distribute data gathered from different sources. This is a one-stop shop to build and deploy your own project with easy to follow instructions."/>
-      			<title>David Jaimes | macOS</title>
-			</Helmet>
-
-			<Navigation></Navigation>
-
-			<Container fluid>
-				<Row className="mt-5 justify-content-center ml-0 mr-0">
-					<Col style={{maxWidth: "768px"}}>
-						<h1 className="mt-5 text-muted text-center"><b className="text-dark">macOS</b> Configuration Setup</h1>
-						<p className="mt-5 mb-5 text-muted">Setup a development environment on a Mac computer from Apple. This documentation is intended for everyone to use as a reference for setting up an environment or installing programming languages and libraries.</p>
-
-						<CardIndex></CardIndex>
-					</Col>
-				</Row>
-			</Container>
-
-			<Footer></Footer>
-			
-		</React.Fragment>
-	)
+const IndexPage = ({data}) => {
+	const Posts = data.allMdx.edges
+    	.filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    	.map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+	return <div>{Posts}</div>
 }
+
+export default IndexPage
+
+
+export const pageQuery = graphql`
+  query {
+    allMdx(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+			path
+          }
+        }
+      }
+    }
+  }
+`
