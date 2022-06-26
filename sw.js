67f7885f29e1a690c33ee24757e3dfd9dbbc49cc
David Jaimes <dj@davidjaimes.com>
2022-06-26 11:10:28 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-683b55a1892ca50d11cc.js"
+    "url": "webpack-runtime-79668d4ca8658e1b8312.js"
   },
   {
     "url": "framework-e9fa771d2049c7b7eba3.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f504dedd315b33ab64c4042d119787c4"
+    "revision": "8e3dfbe71efbe080e786ff2b2e18b638"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-61a3e2a5523eb25bb478.js"
